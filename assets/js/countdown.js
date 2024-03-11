5368eecc60f2cd97181ebf3763a6dc6f6d576f0f
Kolbjørn Bølgen <skaugjr@gmail.com>
2024-03-11 11:31:06 +0100

    pp

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -1,28 +1,17 @@
 
 $(document).ready(function() {
-    var weddingDate = new Date('2025-07-26T15:00:00'); // Set your wedding date and time
+    var weddingDate = new Date('2025-07-26T15:00:00'); // Set your wedding date
 
-    // Format the wedding date and time
-    var formattedWeddingDateTime = weddingDate.toLocaleString('nb-NO', {
-        weekday: 'long',
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric',
-        hour: 'numeric',
-        minute: 'numeric'
-    });
+    // Display the wedding date above the countdown
+    $('#countdown').before('<p>Bryllupsdato: ' + weddingDate.toLocaleString('nb-NO', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' }) + '</p>');
 
-    // Display the wedding date and time
-    $('#countdown').after('<p>Bryllupsdato og tid: ' + formattedWeddingDateTime + '</p>');
-
-    // Initialize the countdown timer
+    // Initialize the countdown
     $('#countdown').countdown(weddingDate, function(event) {
         $(this).html(event.strftime(
             '<div>%w <span>uker</span></div> ' +
             '<div>%d <span>dager</span></div> ' +
             '<div>%H <span>timer</span></div> ' +
             '<div>%M <span>minutter</span></div> ' +
-            '<div>%S <span>sekunder</span></div>'
-        ));
+            '<div>%S <span>sekunder</span></div>'));
     });
 });
