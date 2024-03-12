435ba3d2f4dabfdf1ee599eb8dd98051e623c3b6
Kolbjørn Bølgen <skaugjr@gmail.com>
2024-03-12 11:35:48 +0100

    pp

--- assets/js/galleri.js
+++ assets/js/galleri.js
@@ -7,3 +7,20 @@ gallery.addEventListener('wheel', function(e) {
     // Scroll the gallery
     gallery.scrollLeft += e.deltaY;
 });
+
+function openFullscreen(element) {
+    var fullscreenView = document.getElementById('fullscreen-view');
+    if (!fullscreenView) {
+        fullscreenView = document.createElement('div');
+        fullscreenView.id = 'fullscreen-view';
+        fullscreenView.innerHTML = '<img src=""><button onclick="closeFullscreen()">Close</button>';
+        document.body.appendChild(fullscreenView);
+    }
+    fullscreenView.style.display = 'flex';
+    fullscreenView.querySelector('img').src = element.src;
+}
+
+function closeFullscreen() {
+    var fullscreenView = document.getElementById('fullscreen-view');
+    fullscreenView.style.display = 'none';
+}
