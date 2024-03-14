51604f4922c045a89fa7ccf4b9b8769251c09d13
Kolbjørn Bølgen <skaugjr@gmail.com>
2024-03-14 18:17:00 +0100

    pp

---
+++ assets/js/displayGalleri.js
@@ -0,0 +1,35 @@
+import { ref, getDownloadURL, listAll } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-storage.js";
+import { storage } from './assets/js/firebaseInit.js';
+
+const galleriRef = ref(storage, 'Galleri');
+
+listAll(galleriRef)
+  .then((res) => {
+    res.items.forEach((itemRef) => {
+      // Called once for each item in the folder
+      displayImage(itemRef);
+    });
+  })
+  .catch((error) => {
+    // Handle any errors
+    console.error('Error getting files:', error);
+  });
+
+  function displayImage(itemRef) {
+    getDownloadURL(itemRef)
+      .then((url) => {
+        // Get a reference to the image container
+        const imageContainer = document.getElementById('displayGalleri');
+  
+        // Create an img element
+        const img = document.createElement('img');
+        img.src = url;
+  
+        // Add the img element to the image container
+        imageContainer.appendChild(img);
+      })
+      .catch((error) => {
+        // Handle any errors
+        console.error('Error displaying image:', error);
+      });
+  }
--- assets/js/firebaseInit.js
+++ assets/js/firebaseInit.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-analytics.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-storage.js";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,3 +21,4 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const storage = getStorage(app);
