b51cffc75bd28f1988c7bc2435c483394ff7a40b
Kolbjørn Bølgen <skaugjr@gmail.com>
2024-03-14 21:29:48 +0100

    pp

--- assets/js/displayGalleri.js
+++ assets/js/displayGalleri.js
@@ -17,26 +17,11 @@ listAll(galleriRef)
         const slider = $('.your-slider');
       
         slider.slick({
+          centerMode: true,
+          centerPadding: '60px',
           slidesToShow: 3,
-          slidesToScroll: 1,
-          arrows: true,
+          variableWidth: true,
           responsive: [
-            {
-              breakpoint: 1024,
-              settings: {
-                slidesToShow: 3,
-                slidesToScroll: 1,
-                arrows: true
-              }
-            },
-            {
-              breakpoint: 600,
-              settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1,
-                arrows: true
-              }
-            },
             {
               breakpoint: 480,
               settings: {
