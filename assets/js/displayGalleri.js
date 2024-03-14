42a348be45a8fe9c0ddee886050c86f5ad3d291a
Kolbjørn Bølgen <skaugjr@gmail.com>
2024-03-14 18:23:04 +0100

    Update displayGalleri.js

--- assets/js/displayGalleri.js
+++ assets/js/displayGalleri.js
@@ -1,5 +1,5 @@
 import { ref, getDownloadURL, listAll } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-storage.js";
-import { storage } from './assets/js/firebaseInit.js';
+import { storage } from '.firebaseInit.js';
 
 const galleriRef = ref(storage, 'Galleri');
 
