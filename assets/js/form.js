be5c6b9390ce097d291266e2ffc2ba2a05f643de
Kolbjørn Bølgen <skaugjr@gmail.com>
2024-03-15 11:50:31 +0100

    Update form.js

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,21 +1,7 @@
 // Import Firebase
-import { ref, set, child } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-database.js";
-import { getDownloadURL } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-storage.js";
-import { storage, db } from './firebaseInit.js'; // replace with the actual path to your firebaseInit.js file
+import { get, ref, set, child } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-database.js";
+import { db } from './firebaseInit.js'; // replace with the actual path to your firebaseInit.js file
 
-// Fetch the text file from Firebase Storage
-let invitedGuests = [];
-const fileRef = ref(storage, 'InviterteGjester.txt');
-getDownloadURL(fileRef)
-    .then(url => fetch(url))
-    .then(response => response.text())
-    .then(data => {
-        // Split the file into lines to get an array of names
-        invitedGuests = data.split('\n');
-    })
-    .catch(error => {
-        console.error("Error fetching text file: ", error);
-    });
 
 // Function to submit the form data
 function submitForm(e) {
@@ -29,15 +15,6 @@ function submitForm(e) {
     const additionalGuests = [];
     for (let i = 1; i <= numAdditionalGuests; i++) {
       additionalGuests.push(document.getElementById('additionalGuest' + i).value);
-    }
-
-    // Check if primaryName and each name in additionalGuests are in the list of invited guests
-    const uninvitedGuests = [primaryName, ...additionalGuests].filter(name => !invitedGuests.includes(name));
-
-    if (uninvitedGuests.length > 0) {
-      // If there are uninvited guests, show an error message and stop the form submission
-      alert(`The following guests are not invited: ${uninvitedGuests.join(', ')}`);
-      return;
     }
   
     const newResponseKey = Date.now().toString(); // Generate a unique key based on the current timestamp
@@ -58,7 +35,7 @@ function submitForm(e) {
       console.error("Error adding document: ", error);
       alert("Feilmelding, vennligst prøv på nytt.");
     });
-}
+  }
 
 document.addEventListener('DOMContentLoaded', function() {
   // Event listener for form submission
