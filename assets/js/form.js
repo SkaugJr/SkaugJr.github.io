38a64639771d73213f4671c242bbbd0715374950
Kolbjørn Bølgen <skaugjr@gmail.com>
2024-03-15 14:42:44 +0100

    Update form.js

--- assets/js/form.js
+++ assets/js/form.js
@@ -48,6 +48,8 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Add textboxes for additional guests, starting from 1
     if (numGuests > 0) {
+      const fieldsContainer = document.createElement('div');
+      fieldsContainer.className = 'fields';
       for (let i = 1; i <= numGuests; i++) {
         const newField = document.createElement('div');
         newField.className = 'field';
