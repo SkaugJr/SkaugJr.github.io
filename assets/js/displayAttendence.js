270e33b918341b3b05e8846af95dbbd2ba6169cc
Kolbjørn Bølgen <skaugjr@gmail.com>
2024-03-15 15:29:18 +0100

    Update displayAttendence.js

--- assets/js/displayAttendence.js
+++ assets/js/displayAttendence.js
@@ -35,36 +35,45 @@ get(child(ref(db), 'Svarskjema/')).then((snapshot) => {
     }
 
     // Create a string for the HTML output
-    var html = '<table><thead><tr><th>Aida</th><th>Kolbjørn</th></tr></thead><tbody>';
+    // Create a string for the HTML output
+var html = '<table style="width: 100%; text-align: center;"><thead><tr><th colspan="2">Aida</th><th colspan="2">Kolbjørn</th></tr></thead><tbody>';
 
-    // Find the maximum length between the two arrays
-    var maxLength = Math.max(aida.length, kolbjorn.length);
+// Find the maximum length between the two arrays
+var maxLength = Math.max(aida.length, kolbjorn.length);
 
-    // Loop through the maxLength
-    for (var i = 0; i < maxLength; i++) {
-      html += '<tr>';
+// Loop through the maxLength
+for (var i = 0; i < maxLength; i++) {
+  html += '<tr>';
 
-      // If there is a name in the aida array at this index, add it to the table
-      if (i < aida.length) {
-        html += '<td>' + aida[i] + '</td>';
-      } else {
-        html += '<td></td>'; // Add an empty cell if there is no name
-      }
+  // If there is a name in the aida array at this index, add it to the table
+  if (i < aida.length) {
+    html += '<td colspan="2">' + aida[i] + '</td>';
+  } else {
+    html += '<td colspan="2"></td>'; // Add an empty cell if there is no name
+  }
 
-      // If there is a name in the kolbjorn array at this index, add it to the table
-      if (i < kolbjorn.length) {
-        html += '<td>' + kolbjorn[i] + '</td>';
-      } else {
-        html += '<td></td>'; // Add an empty cell if there is no name
-      }
+  // If there is a name in the kolbjorn array at this index, add it to the table
+  if (i < kolbjorn.length) {
+    html += '<td colspan="2">' + kolbjorn[i] + '</td>';
+  } else {
+    html += '<td colspan="2"></td>'; // Add an empty cell if there is no name
+  }
 
-      html += '</tr>';
-    }
+  html += '</tr>';
 
-    html += '</tbody></table>';
+  // If there are additional guests, add them in the next row in the second column of the pair
+  if (aida[i] && aida[i].additionalGuests) {
+    html += '<tr><td></td><td>' + aida[i].additionalGuests.join(', ') + '</td><td colspan="2"></td></tr>';
+  }
+  if (kolbjorn[i] && kolbjorn[i].additionalGuests) {
+    html += '<tr><td colspan="2"></td><td></td><td>' + kolbjorn[i].additionalGuests.join(', ') + '</td></tr>';
+  }
+}
 
-    // Update the HTML of a specific element to display the list
-    document.getElementById('nameList').innerHTML = html;
+html += '</tbody></table>';
+
+// Update the HTML of a specific element to display the list
+document.getElementById('nameList').innerHTML = html;
   } else {
     console.log("No data available");
   }
