554568653db1fe5b2fd54791180c86c39875d2e2
Kolbjørn Bølgen <skaugjr@gmail.com>
2024-03-15 15:11:08 +0100

    push

--- assets/js/displayAttendence.js
+++ assets/js/displayAttendence.js
@@ -1,48 +1,55 @@
 import { db } from './firebaseInit.js'; // Adjust the path based on the actual location of firebaseInit.js
+import { get, ref } from 'https://www.gstatic.com/firebasejs/10.9.0/firebase-database.js';
 
 // Retrieve the data from Firebase
-db.ref('/').once('value').then(function(snapshot) {
-  var data = snapshot.val();
+get(ref(db, '/')).then((snapshot) => {
+  if (snapshot.exists()) {
+    var data = snapshot.val();
 
-  // Create an empty object for each relation
-  var aida = [];
-  var kolbjorn = [];
+    // Create an empty object for each relation
+    var aida = [];
+    var kolbjorn = [];
 
-  // Loop through the data
-  for (var key in data) {
-    if (data.hasOwnProperty(key)) {
-      var entry = data[key];
+    // Loop through the data
+    for (var key in data) {
+      if (data.hasOwnProperty(key)) {
+        var entry = data[key];
 
-      // Add the primaryName to the appropriate relation object
-      if (entry.familyRelation === 'Aida') {
-        aida.push(entry.primaryName);
-      } else if (entry.familyRelation === 'Kolbjørn') {
-        kolbjorn.push(entry.primaryName);
-      }
-
-      // If there are any additionalGuests, add them as well
-      if (entry.additionalGuests) {
-        var additionalGuests = Array.isArray(entry.additionalGuests) ? entry.additionalGuests : entry.additionalGuests.split(', ');
+        // Add the primaryName to the appropriate relation object
         if (entry.familyRelation === 'Aida') {
-          aida = aida.concat(additionalGuests);
+          aida.push(entry.primaryName);
         } else if (entry.familyRelation === 'Kolbjørn') {
-          kolbjorn = kolbjorn.concat(additionalGuests);
+          kolbjorn.push(entry.primaryName);
+        }
+
+        // If there are any additionalGuests, add them as well
+        if (entry.additionalGuests) {
+          var additionalGuests = Array.isArray(entry.additionalGuests) ? entry.additionalGuests : entry.additionalGuests.split(', ');
+          if (entry.familyRelation === 'Aida') {
+            aida = aida.concat(additionalGuests);
+          } else if (entry.familyRelation === 'Kolbjørn') {
+            kolbjorn = kolbjorn.concat(additionalGuests);
+          }
         }
       }
     }
-  }
 
-  // Create a string for the HTML output
-  var html = '<h2>Aida</h2><ul>';
-  for (var i = 0; i < aida.length; i++) {
-    html += '<li>' + aida[i] + '</li>';
-  }
-  html += '</ul><h2>Kolbjørn</h2><ul>';
-  for (var i = 0; i < kolbjorn.length; i++) {
-    html += '<li>' + kolbjorn[i] + '</li>';
-  }
-  html += '</ul>';
+    // Create a string for the HTML output
+    var html = '<h2>Aida</h2><ul>';
+    for (var i = 0; i < aida.length; i++) {
+      html += '<li>' + aida[i] + '</li>';
+    }
+    html += '</ul><h2>Kolbjørn</h2><ul>';
+    for (var i = 0; i < kolbjorn.length; i++) {
+      html += '<li>' + kolbjorn[i] + '</li>';
+    }
+    html += '</ul>';
 
-  // Update the HTML of a specific element to display the list
-  document.getElementById('nameList').innerHTML = html;
+    // Update the HTML of a specific element to display the list
+    document.getElementById('nameList').innerHTML = html;
+  } else {
+    console.log("No data available");
+  }
+}).catch((error) => {
+  console.error(error);
 });
