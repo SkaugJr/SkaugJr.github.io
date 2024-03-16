d169c818b0f6b33ae74181a66aaffcca9695b753
Kolbjørn Bølgen <skaugjr@gmail.com>
2024-03-16 21:59:36 +0100

    test

---
+++ assets/js/login.js
@@ -0,0 +1,21 @@
+// Retrieve reference to the Firebase Realtime Database
+const db = firebase.database();
+const usersRef = db.ref('/Brukere');
+
+// Login function
+function loginUser() {
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
+
+  // Check if the username exists in the database
+  usersRef.child(username).once('value', (snapshot) => {
+    const userData = snapshot.val();
+    if (userData && userData.password === password) {
+      // Authentication successful, redirect to the dashboard or home page
+      window.location.href = 'index.html'; // Replace 'dashboard.html' with the desired destination
+    } else {
+      // Authentication failed, display an error message
+      alert('Invalid username or password. Please try again.');
+    }
+  });
+}
