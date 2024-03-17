eee268ab8e6d69554071fd3a01299cd73b4b3a5f
Kolbjørn Bølgen <skaugjr@gmail.com>
2024-03-17 11:55:24 +0100

    Update gallery.js

--- Galleri/assets2/js/gallery.js
+++ Galleri/assets2/js/gallery.js
@@ -3,41 +3,42 @@ import { ref, getDownloadURL, listAll } from "https://www.gstatic.com/firebasejs
 
 // Function to create a thumbnail from a full-sized image
 function createThumbnail(imageUrl, maxWidth, maxHeight) {
-  return new Promise((resolve, reject) => {
-    var img = new Image();
-    img.onload = function() {
-      var canvas = document.createElement('canvas');
-      var ctx = canvas.getContext('2d');
-
-      var width = img.width;
-      var height = img.height;
-
-      // Calculate the new dimensions while maintaining aspect ratio
-      if (width > height) {
-        if (width > maxWidth) {
-          height *= maxWidth / width;
-          width = maxWidth;
+    return new Promise((resolve, reject) => {
+      var img = new Image();
+      img.crossOrigin = "anonymous"; // Add this line
+      img.onload = function() {
+        var canvas = document.createElement('canvas');
+        var ctx = canvas.getContext('2d');
+  
+        var width = img.width;
+        var height = img.height;
+  
+        // Calculate the new dimensions while maintaining aspect ratio
+        if (width > height) {
+          if (width > maxWidth) {
+            height *= maxWidth / width;
+            width = maxWidth;
+          }
+        } else {
+          if (height > maxHeight) {
+            width *= maxHeight / height;
+            height = maxHeight;
+          }
         }
-      } else {
-        if (height > maxHeight) {
-          width *= maxHeight / height;
-          height = maxHeight;
-        }
-      }
-
-      canvas.width = width;
-      canvas.height = height;
-
-      ctx.drawImage(img, 0, 0, width, height);
-
-      resolve(canvas.toDataURL());
-    };
-    img.onerror = function() {
-      reject(new Error("Failed load image"));
-    };
-    img.src = imageUrl;
-  });
-}
+  
+        canvas.width = width;
+        canvas.height = height;
+  
+        ctx.drawImage(img, 0, 0, width, height);
+  
+        resolve(canvas.toDataURL());
+      };
+      img.onerror = function() {
+        reject(new Error("Failed load image"));
+      };
+      img.src = imageUrl;
+    });
+  }
 
 // Fetch all the images and display them
 var listRef = ref(storage, 'Galleri'); // Adjust this path to your images
