1ff6a195b2bc85fd25632c684ccb7a9b0e1b2829
Kolbjørn Bølgen <skaugjr@gmail.com>
2024-03-19 00:36:51 +0100

    test

--- Galleri/assets2/js/gallery.js
+++ Galleri/assets2/js/gallery.js
@@ -50,8 +50,7 @@ function displayImage(url, imageNumber) {
 
 function openSlider(selector) {
   $(selector).poptrox({
-      usePopupCaption: true,
-      usePopupNav: true
+      usePopupCaption: true
   });
 }
 
