837cdb4586deb04e2a12533449af57675fd7848d
Kolbjørn Bølgen <skaugjr@gmail.com>
2024-03-19 18:35:47 +0100

    test

--- assets/js/form.js
+++ assets/js/form.js
@@ -54,7 +54,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
         const newLabel = document.createElement('label');
         newLabel.htmlFor = 'additionalGuest' + i;
-        newLabel.textContent = 'Ekstra gjest ' + i;
+        newLabel.textContent = 'Medhørende gjest ' + i;
 
         const newInput = document.createElement('input');
         newInput.type = 'text';
