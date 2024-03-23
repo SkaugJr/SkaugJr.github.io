6c0d28360b651ec5db203c11dc41d64b3585b4ce
Kolbjørn Bølgen <skaugjr@gmail.com>
2024-03-23 17:46:43 +0100

    test

---
+++ assets/js/displayGjestebok.js
@@ -0,0 +1,27 @@
+import { db } from './firebaseInit.js'; // Adjust the path based on the actual location of firebaseInit.js
+import { get, ref, child } from 'https://www.gstatic.com/firebasejs/10.9.0/firebase-database.js';
+
+// Get a reference to the Gjestebok folder in Firebase
+const gjestebokRef = ref(db, 'Gjestebok');
+
+// Use the get function to retrieve the data
+get(gjestebokRef)
+  .then((snapshot) => {
+    if (snapshot.exists()) {
+      const data = snapshot.val();
+      const messagesContainer = document.getElementById('messages');
+
+      // Loop through the data and create HTML elements for each message
+      for (let key in data) {
+        const message = data[key].message;
+        const messageElement = document.createElement('p');
+        messageElement.textContent = message;
+        messagesContainer.appendChild(messageElement);
+      }
+    } else {
+      console.log("No data available");
+    }
+  })
+  .catch((error) => {
+    console.error(error);
+  });
