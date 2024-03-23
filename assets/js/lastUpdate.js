1351d2515ee949c125dd6ed8a41e1e94d66b864e
Kolbjørn Bølgen <skaugjr@gmail.com>
2024-03-23 17:43:28 +0100

    test

--- assets/js/lastUpdate.js
+++ assets/js/lastUpdate.js
@@ -3,6 +3,6 @@ fetch('https://api.github.com/repos/SkaugJr/SkaugJr.github.io/commits?path=index
     .then(data => {
         const lastUpdated = new Date(data[0].commit.author.date);
         const options = { day: '2-digit', month: '2-digit', year: 'numeric', hour: '2-digit', minute: '2-digit' };
-        document.getElementById('last-updated').innerHTML = '<i class="fa-solid fa-clock-rotate-left"></i> Sist oppdatering: ' + lastUpdated.toLocaleString('nb-NO', options);
+        document.getElementById('last-updated').textContent = 'Sist oppdatering: ' + lastUpdated.toLocaleString('nb-NO', options);
     })
     .catch(error => console.error(error));
