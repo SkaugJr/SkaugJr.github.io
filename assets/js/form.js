0b65db5b0e0a72ece10c7fbadee01b9e3a4cb722
Kolbjørn Bølgen <skaugjr@gmail.com>
2024-03-26 13:12:39 +0100

    Update form.js

--- assets/js/form.js
+++ assets/js/form.js
@@ -16,7 +16,7 @@ function submitForm(e) {
     additionalGuests.push(document.getElementById('additionalGuest' + i).value);
   }
 
-  const newResponseKey = Date.now().toString(); + '_' + primaryName  // Generate a unique key based on the current timestamp
+  const newResponseKey = Date.now().toString() + '_' + primaryName;  // Generate a unique key based on the current timestamp
 
   const participationFolder = participation === '1' ? 'Deltar' : 'Deltar Ikke';
 
