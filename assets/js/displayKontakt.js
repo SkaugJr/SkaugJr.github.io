b22bc2d57bc6fc0bc2b4755d83c85687d8e40343
Kolbjørn Bølgen <skaugjr@gmail.com>
2024-03-26 13:23:10 +0100

    Update displayKontakt.js

--- assets/js/displayKontakt.js
+++ assets/js/displayKontakt.js
@@ -3,34 +3,38 @@ import { get, ref, child } from 'https://www.gstatic.com/firebasejs/10.9.0/fireb
 
 // Check if the current user is an admin
 auth.onAuthStateChanged((user) => {
-    if (user && user.email === 'admin@akbryllup.no') {
-      const kontaktRef = ref(db, 'Kontakt');
-  
-      get(kontaktRef).then((snapshot) => {
-        if (snapshot.exists()) {
-          const data = snapshot.val();
-          const ul = document.createElement('ul');
+  if (user && user.email === 'admin@akbryllup.no') {
+    const kontaktRef = ref(db, 'Kontakt');
 
-          const header = document.createElement('h3');
-          header.innerHTML = '<i class="fa-solid fa-inbox"></i> Beskjeder:';
-  
-          for (let key in data) {
-            const li = document.createElement('li');
-            li.textContent = data[key].name;
-  
-            const p = document.createElement('p');
-            p.textContent = data[key].message;
-  
-            li.appendChild(p);
-            ul.appendChild(li);
-          }
-  
-          document.getElementById('kontaktMeldinger').appendChild(ul);
-        } else {
-          console.log("No data available");
+    get(kontaktRef).then((snapshot) => {
+      if (snapshot.exists()) {
+        const data = snapshot.val();
+        const ul = document.createElement('ul');
+
+        // Create a header
+        const header = document.createElement('h3');
+        header.innerHTML = '<i class="fa-solid fa-inbox"></i> Beskjeder:';
+
+        for (let key in data) {
+          const li = document.createElement('li');
+          li.textContent = data[key].name;
+
+          const p = document.createElement('p');
+          p.textContent = data[key].message;
+
+          li.appendChild(p);
+          ul.appendChild(li);
         }
-      }).catch((error) => {
-        console.error(error);
-      });
-    }  
-  });
+
+        // Append the header and the list to 'kontaktMeldinger'
+        const kontaktMeldinger = document.getElementById('kontaktMeldinger');
+        kontaktMeldinger.appendChild(header);
+        kontaktMeldinger.appendChild(ul);
+      } else {
+        console.log("No data available");
+      }
+    }).catch((error) => {
+      console.error(error);
+    });
+  }  
+});
