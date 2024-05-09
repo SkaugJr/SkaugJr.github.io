d6c5ef73d7b3dc7a6664c1accc6e0540d567c35a
Kolbjørn Bølgen <skaugjr@gmail.com>
2024-05-09 22:18:09 +0200

    Update firebaseInit.js

--- assets/js/firebaseInit.js
+++ assets/js/firebaseInit.js
@@ -6,6 +6,17 @@ import { getStorage } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-s
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-database.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js";
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyArOXQKdmFjragCtWHgrQglooNDlgUfXpk",
+  authDomain: "ak-bryllup.firebaseapp.com",
+  databaseURL: "https://ak-bryllup-default-rtdb.europe-west1.firebasedatabase.app/",
+  projectId: "ak-bryllup",
+  storageBucket: "ak-bryllup.appspot.com",
+  messagingSenderId: "219159117972",
+  appId: "1:219159117972:web:2531f93e1d22d7f04a3481",
+  measurementId: "G-Y3TG6Y30BE"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
